93f0311bde0942c4ce61b63d8b6da5da3a0248de
jormaraty <jorma.raty@metropolia.fi>
2024-01-25 12:15:11 +0200

    Ohjelman idea selvillä, toteutus vähän kesken...

---
+++ mod-04/teht2.py
@@ -0,0 +1,18 @@
+'''
+Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan
+kunnes käyttäjä antaa negatiivisen tuumamäärän.
+Sen jälkeen ohjelma lopettaa toimintansa. 1 tuuma = 2,54 cm
+'''
+
+# Kysyn tuumien määrän yhden kerran ennen toistoa
+# Nyt käyttäjä voi antaa myös desimaaliluvun
+tuumat = float( input("Anna tuumien määrä: ") )
+
+# tehdään tähän while-toisto, joka loppuu
+# kun saadaan negatiivinen tuumien määrä
+
+    # while-toiston sisällä: muutetaan tuumat senteiksi ja
+    # tulostetaan senttien lukumäärä.
+    # Muista kysyä uusi tuumien lukumäärä!
+
+print("Hyvää päivän jatkoa!")
